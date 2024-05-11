28825e2c9edd659c51ebde1cecc19c074a71782c
Natalia Espinosa <nataliage18@gmail.com>
2024-05-11 12:23:05 -0600

    Se agrega el proyecto

---
+++ ToDoList/ToDoCell.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+protocol ToDoCellDelegate {
+    func checkMarkTapped(_ sender: ToDoCell)
+}
+
+class ToDoCell: UITableViewCell {
+    
+    @IBOutlet weak var isCompleteButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var delegate: ToDoCellDelegate?
+    
+    @IBAction func isCompleteButtonTapped(_ sender: UIButton) {
+        delegate?.checkMarkTapped(self)
+    }
+}
